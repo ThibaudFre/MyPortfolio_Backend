3d4885cf028aaa0cc9c2ca42c7c51f87547b5fa3
Thibaud <thibaudfresnay.pro@gmail.com>
2025-01-10 14:14:26 +0100

    models added to models.py

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+print(include("polls.urls"))
+
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
