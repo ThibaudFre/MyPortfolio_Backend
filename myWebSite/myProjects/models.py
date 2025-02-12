05ddd8b9de34434f0d94d50fd1c6217a1ef67aef
Thibaud <thibaudfresnay.pro@gmail.com>
2025-02-12 16:27:08 +0100

    migrate command launched models.py updated

--- myWebSite/myProjects/models.py
+++ myWebSite/myProjects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Project(models.Model):
+    PERSONAL = "PERS"
+    PROFESSIONAL = "PRO"
+    FORMATION = "FORM"
+    
+    TYPE_OF_PROJECT_CHOICES = {
+        "PERSONAL": "Personel",
+        "PROFESSIONAL": "Professionel",
+        "FORMATION": "Formation",
+    }
+    
+    project_title = models.CharField(max_length=100)
+    project_type = models.Charfield(choices =TYPE_OF_PROJECT_CHOICES, default= PERSONAL)
+    project_text = models.TextField()
 # Create your models here.
