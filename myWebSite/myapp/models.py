3f79740ba96b219b1f820cac65e5782342ab0dfb
Thibaud Fresnay <thibaudfresnay.pro@gmail.com>
2025-04-29 14:45:09 +0200

    There was a bug that I fixed

---
+++ myWebSite/myapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Project(models.Model):
+    PERSONAL = "PERS"
+    PROFESSIONAL = "PRO"
+    FORMATION = "FORM"
+    
+    TYPE_OF_PROJECT_CHOICES = {
+        "PERSONAL": "Personel",
+        "PROFESSIONAL": "Professionel",
+        "FORMATION": "Formation",
+    }
+    
+    
+    project_title = models.CharField(max_length=100)
+    project_short_text = models.TextField(default="")
+    project_text = models.TextField(default="")
+    project_link = models.TextField(default="")
+    project_client = models.TextField(default="")
+    project_github_link = models.TextField(default="")
+    project_image = models.ImageField(upload_to = "projects")
+    project_type = models.CharField(choices =TYPE_OF_PROJECT_CHOICES, default= PERSONAL, max_length=100)
+    project_date = models.DateTimeField("date published")
+    
+    def __str__ (self):
+        return self.project_title
+# Create your models here.
